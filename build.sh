04ee82468688bf50a4e812ed3c62e3b91daf954a
Mihail Binev <kodeart@gmail.com>
2020-02-12 10:49:16 +0100

    updates build script

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir -p releases
+
 GOOS=darwin GOARCH=amd64 go build -o acolytes && zip -r ./releases/acolytes-darwin ./images ./acolytes && rm acolytes \
   && GOOS=linux GOARCH=amd64 go build -o acolytes && zip -r ./releases/acolytes-linux ./images ./acolytes && rm acolytes \
   && GOOS=windows GOARCH=amd64 go build -o acolytes.exe && zip -r ./releases/acolytes-win ./images ./acolytes.exe && rm acolytes.exe
